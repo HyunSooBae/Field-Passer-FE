195bcd63a549d394565c4fba4f62378c4fde160c
iziz9 <46iz.us2@gmail.com>
2023-01-23 16:47:39 +0900

    Chore: Project setting

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+  <div className="w-full h-28 bg-sky-700 text-white text-6xl">
+    <span className="block w-fit m-auto"> HEADER </span>
+  </div>
+  )
+};
+
+export default Header;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
+  content: ['./src/**/*.jsx'],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xs: '400px',
+        sm: '540px',
+        md: '800px',
+        lg: '1024px',
+        xl: '1280px',
+      },
+      fontSize: {
+        xxs: '0.6rem',
+        xs: '0.75rem',
+        sm: '0.875rem',
+        base: '1rem',
+        xl: '1.25rem',
+      },
+    },
   },
-  plugins: [],
 };
