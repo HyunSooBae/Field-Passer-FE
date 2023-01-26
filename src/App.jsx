6eae3199354fc4a955f2178fa7b55370f05bae33
quokka-eating-carrots <ovzip7@gmail.com>
2023-01-26 15:04:07 +0900

    Feat: Json-server, axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
 import './App.css';
+import { Outlet, ScrollRestoration } from 'react-router-dom';
+import requestAPI from './api/axios';
 
 function App() {
+  useEffect(() => {
+    requestAPI();
+  }, []);
   return (
     <div>
+      <ScrollRestoration />
       <Header />
+      <Outlet />
     </div>
   );
 }
