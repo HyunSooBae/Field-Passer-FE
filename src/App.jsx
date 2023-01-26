0a578a7d1f1fe4e226287f8afaca50f88bfab9da
quokka-eating-carrots <ovzip7@gmail.com>
2023-01-26 14:43:02 +0900

    Feat: router, json test server

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,13 @@
 import React, { useState } from 'react';
 import Header from './components/Header';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import Board from './pages/Board';
-import MyPage from './pages/MyPage';
-import Admin from './pages/Admin';
-import NotFound from './pages/NotFound';
-import './App.css'
+import './App.css';
 
 function App() {
-
   return (
-    <BrowserRouter>
+    <div>
       <Header />
-      <div>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/board/*' element={<Board />} />
-          <Route path='/mypage/*' element={<MyPage />} />
-          <Route path='/admin/*' element={<Admin />} />
-          <Route path='/*' element={<NotFound />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    </div>
   );
 }
 
-export default App
+export default App;
