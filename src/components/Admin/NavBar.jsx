deffc39bd6a979d43ffae2e3e28e7b87adf7bd06
quokka-eating-carrots <ovzip7@gmail.com>
2023-01-27 15:26:58 +0900

    Feat: Admin 페이지 NavBar

--- src/components/Admin/NavBar.jsx
+++ src/components/Admin/NavBar.jsx
@@ -1,7 +1,36 @@
 import React from 'react';
+import NavTab from './NavTab';
+import { BsPeopleFill } from 'react-icons/bs';
+import { HiDocumentDuplicate } from 'react-icons/hi';
+import { RiCustomerService2Fill } from 'react-icons/ri';
+import { CgWebsite } from 'react-icons/cg';
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  const iconClass = 'cursor-pointer w-20 h-20 text-white';
+  const member = () => <BsPeopleFill className={iconClass} />;
+  const post = () => <HiDocumentDuplicate className={iconClass} />;
+  const service = () => <RiCustomerService2Fill className={iconClass} />;
+  const main = () => <CgWebsite className={iconClass} />;
+  return (
+    <div className='flex bg-field flex-col box-border h-screen justify-center items-center gap-5 w-96'>
+      <img src='./images/anony.png' alt='' className='w-24' />
+      <div className='flex gap-5 text-white'>
+        <span>관리자 ADMIN</span>
+        <span>|</span>
+        <button>로그아웃</button>
+      </div>
+      <div>
+        <div className='flex gap-5 p-5 flex-wrap justify-center'>
+          <NavTab title='회원 관리' icon={member()} />
+          <NavTab title='게시글 관리' icon={post()} />
+        </div>
+        <div className='flex gap-5 p-5 flex-wrap justify-center'>
+          <NavTab title='고객센터' icon={service()} />
+          <NavTab title='FIELD-PASSER' icon={main()} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import NavBar from '../components/Admin/NavBar';
+import Main from '../components/Admin/Main';
 
 const Admin = () => {
   return (
     <div>
       <NavBar />
+      <Main />
     </div>
   );
 };
