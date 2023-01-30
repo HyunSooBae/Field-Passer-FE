d749244fd131a9869ed0e9ee8e34f241c9199550
Hyeonju Kang <46iz.us2@gmail.com>
2023-01-30 15:03:54 +0900

    Design: Modify responsive searchbar UI

--- src/components/home/search/Searchbar.jsx
+++ src/components/home/search/Searchbar.jsx
@@ -8,11 +8,11 @@ const Searchbar = () => {
   const stadiumName = ['체육관', '운동장', '스타디움', '공원', '실내구장']
 
   return (
-    <section className="xxs:mt-12 xxs:flex-col xxs:m-auto xxs:flex sm:flex-row bg-zinc-200 py-2 px-2 items-center rounded-lg">
+    <section className="xs:w-96 sm:w-fit xxs:mt-12 xxs:flex-col xxs:m-auto xxs:flex sm:flex-row bg-zinc-200 py-2 px-2 items-center rounded-lg">
       <SelectBox id="category" names={categoryName} defaultValue="종목" size="w-28" />
       <SelectBox id="district" names={districtName} defaultValue="지역" size="w-28" />
       <SelectBox id="stadium" names={stadiumName} defaultValue="구장" size="w-60"/>
-      <button type="submit" className="w-16 h-10 xxs:mt-1 sm:mt-0 sm:ml-1 rounded-lg bg-field font-bold text-white">
+      <button type="submit" className="w-16 h-10 xxs:mt-1 sm:mt-0 sm:ml-1 rounded-lg bg-field font-bold hover:bg-hoverField text-white">
         검색
       </button>
     </section>
