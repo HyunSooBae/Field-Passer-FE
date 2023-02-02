5088416df39b57f7716401d9ddf228895aed0020
0nesan <0nesan.o8o@gmail.com>
2023-02-02 14:55:46 +0900

    feat : searchbar 컴포넌트 병합, 상세 페이지 지도 api 삽입, 검색 컴포넌트 기능개선

--- src/pages/BoradForm.jsx
+++ src/pages/BoradForm.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import Datepicker from "tailwind-datepicker-react"
 
 const BoradForm = () => {
+  //datepicker options
   const options = {
     title: "예약 일자",
     autoHide: true,
@@ -20,7 +21,6 @@ const BoradForm = () => {
       selected: "text-white bg-field hover:bg-field",
     },
     icons: {
-      // () => ReactNode | JSX.Element
       prev: () => <span>{'<'}</span>,
       next: () => <span>{'>'}</span>,
     },
@@ -29,7 +29,8 @@ const BoradForm = () => {
     language: "ko",
   }
   
-  const DemoComponent = () => {
+  // Datepicker component
+  const FormDatepicker = () => {
     const [show, setShow] = useState(false)
     const handleChange = (selectedDate) => {
       
@@ -45,10 +46,9 @@ const BoradForm = () => {
     )
   }
 
-
   return (
-    <div className='mt-12 mb-24 mx-auto'>
-        <form action="" className='w-[600px] ml-auto mr-auto mt-0 mb-0 flex flex-col gap-5'>
+    <div className='my-[50px] mx-auto px-0 mm:px-[20px]'>
+        <form action="" className='w-full max-w-[600px] ml-auto mr-auto mt-0 mb-0 flex flex-col gap-[10px] mm:gap-[20px]'>
             <div className='flex gap-3'>
                 <select name="" id="" className='w-24 h-8 border-2 border-solid border-#94a3b8 rounded-md text-sm pl-2 focus:outline-none focus:border-field '>
                     <option value="">종목</option>
@@ -60,15 +60,15 @@ const BoradForm = () => {
                     <option value="">구장</option>
                 </select>
             </div>
-            <div className='flex flex-col gap-5'>
-                <input type="text" disabled placeholder='구장' className='h-10 rounded text-sm border-#94a3b8 border-solid border-2 px-3'/>
-                <input type="number" placeholder='가격 입력' className='h-10 rounded text-sm border-#94a3b8 border-solid border-2 px-3 focus:outline-none focus:border-field appearance-none'/>
+            <div className='flex flex-col gap-[10px] mm:gap-[20px]'>
+                <input type="text" placeholder='제목' className='h-10 rounded text-sm border-#94a3b8 border-solid border-2 px-3 focus:outline-none focus:border-field'/>
+                <input type="number" placeholder='가격 입력' className='h-10 rounded text-sm border-#94a3b8 border-solid border-2 px-3 focus:outline-none focus:border-field'/>
                 <textarea placeholder='내용 입력' className='resize-none h-10 rounded text-sm border-#94a3b8 border-solid border-2 px-3 pt-3 h-96 focus:outline-none focus:border-field'></textarea>
             </div>
             <div>
               <p className='text-xs text-gray-600 mb-[10px]'>예약 날짜 / 시간</p>
               <div className='flex'>
-                <DemoComponent />
+                <FormDatepicker />
                 <input
                   type="time"
                   className='form-control block w-[calc(50%-10px)] px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
@@ -81,7 +81,7 @@ const BoradForm = () => {
               <p className='text-xs text-gray-600 mb-[10px]'>인증 사진</p>
               <div>
                 <label htmlFor="images" className='block w-[100px] h-[100px] border-solid border rounded cursor-pointer border-[#ddd]'>카메라 icon</label>
-                <input id="images" type='file' multiple className='hidden'></input>
+                <input id="images" type='file' multiple className='hidden' accept='image/gif,image/jpeg,image/png' />
               </div>
             </div>
 
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import { createHtmlPlugin } from 'vite-plugin-html'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default ({ mode }) => {  
+  const env = loadEnv(mode, process.cwd())
+  // process에서 타입에러가 뜨게된다.
+  // @type/node를 설치해주자
+  return {
+    plugins: [
+      react(),
+      createHtmlPlugin({
+        minify: true,
+        inject: {
+          data: {
+            naverClientId: env.VITE_NAVER_MAP_CLIENT_ID,
+          }
+        }
+      }),
+    ],
+  }
+}
