ff54424142cf49c79b5eb3e5038c44f20b51961d
hyunsooBae <hyunsooBae.dev@gmail.com>
2023-03-27 21:11:13 +0900

    fix: 타입 오류 수정
    - join 페이지 에러메세지 타입 오류 수정

--- src/pages/Join.tsx
+++ src/pages/Join.tsx
@@ -36,15 +36,15 @@ const Join = () => {
       profileImg: z
         .any()
         .default(null)
-        .refine((file) => file !== null, {
-          message: '.jpg, .jpeg, .png, gif 형식에 맞는 파일을 업로드해주세요.',
+        .refine((file) => !file?.[0] || file == '' || file !== null || file == undefined || file.length == 0, {
+          message: '이미지 파일을 업로드해주세요.',
         })
         .refine((file) => ACCEPTED_IMAGE_TYPES.includes(file?.[0]?.type), {
           message: '.jpg, .jpeg, .png, gif 형식에 맞는 파일을 업로드해주세요.',
         })
         .refine((file) => file?.[0]?.size <= MAX_FILE_SIZE, {
           message: `파일 용량은 최대 1MB까지 가능합니다.`,
-        }),
+        })
       // .optional(),
     })
     .required()
@@ -66,6 +66,7 @@ const Join = () => {
   const onSubmit: SubmitHandler<formSchema> = async (data) => {
     console.log(watch(data.profileImg));
     const { email, passwordCheck, memberName, profileImg } = data;
+    console.log(profileImg);
     console.log(profileImg[0]);
     const formData = new FormData();
     formData.append('email', email);
@@ -76,8 +77,8 @@ const Join = () => {
     });
     formData.append('profileImg', profileImg[0]);
     console.log(data);
-    const { ok, authData } = await join(formData);
-    console.log(authData);
+    // const { ok, authData } = await join(formData);
+    // console.log(authData);
     // setConfirmModal(true);
     let entries = formData.entries();
     for (const pair of entries) {
@@ -179,7 +180,7 @@ const Join = () => {
               </label> */}
 
               <div>
-                <div className='flex gap-[10px] relative'>
+                <div className='flex flex-wrap justify-center gap-[10px] relative text-center'>
                   <input
                     id='images'
                     type='file'
@@ -192,9 +193,6 @@ const Join = () => {
                     // ref={imgRef}
                     name='profileImg'
                   />
-                  {/* {errors.profileImg && (
-                    <p className='text-xs text-red-600 py-3'>{errors.profileImg.message}</p>
-                  )} */}
                   {avatarPreview && !errors.profileImg ? (
                     <>
                       <img
@@ -215,6 +213,9 @@ const Join = () => {
                       htmlFor='images'
                       className='block w-[100px] h-[100px] border-solid border rounded-lg cursor-pointer border-[#ddd] bg-[url("/images/cam.png")] bg-center bg-[length:60px] bg-no-repeat'
                     />
+                  )}
+                  {errors.profileImg && (
+                    <p className='w-full text-xs text-red-600 py-3'>{String(errors.profileImg.message)}</p>
                   )}
                 </div>
               </div>
